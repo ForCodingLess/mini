e829fe504c8f409866888d38f272b183bda89e68
Chen <l_beauty@163.com>
2017-12-18 17:29:34 +0800

    New Files

--- lib/js/index.jsx
+++ lib/js/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Extra from "./extra.js";
 import '../style/index.scss';
 
 class Main extends React.Component{
@@ -10,6 +11,8 @@ class Main extends React.Component{
 
 		}
 
+		this.move=this.move.bind(this)
+
 	}
 
 	componentWillMount(){
@@ -17,12 +20,21 @@ class Main extends React.Component{
 	}
 
 	componentDidMount(){
-		
+		this.handler=new Extra.Socket();
+	}
+
+	move(){
+		this.handler.send(~~(Math.random()*10000))
 	}
 
 	render(){
+		const style={
+			"width":"100%",
+			"height":"400px",
+			"backgroundColor":"rgba(112,55,45,0.6)"
+		};
 		return(
-			<div>
+			<div style={style} onMouseMove={this.move}>
 				
 			</div>
 		)
