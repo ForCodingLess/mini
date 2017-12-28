b50beb8a2a9e18e23afd0e7e3826c6a5f11832c7
Chen <l_beauty@163.com>
2017-12-28 14:22:28 +0800

    Change

--- lib/js/index.jsx
+++ lib/js/index.jsx
@@ -11,15 +11,23 @@ class Main extends React.Component{
 			id:"",
 			openid:"",
 			headimg:"",
-			nickname:"",
-			width:"",
-			height:""
+			nickname:""
 		}
 
 		this.init=this.init.bind(this)
-		this.update=this.update.bind(this)
+		this.receive=this.receive.bind(this)
 		this.pushMsgQueue=this.pushMsgQueue.bind(this)
+
+		this.preload=this.preload.bind(this)
+		this.create=this.create.bind(this)
+		this.update=this.update.bind(this)
 	}
+
+	preload(){
+		this.game.load.image('tree1','')
+	}
+	create(){}
+	update(){}
 
 	init(id,openid,nickname,headimg){
 		this.handler=new Extra.Socket({
@@ -28,7 +36,7 @@ class Main extends React.Component{
 			headimg:headimg,
 			nickname:nickname,
 			onMessage:(data)=>{
-				this.update(data)
+				this.receive(data)
 			}
 		})
 	}
@@ -58,32 +66,28 @@ class Main extends React.Component{
 	  }
 	}
 
-	update(res){
+	receive(res){
  		let obj = JSON.parse(res.data)
-  	let _type = obj.code
-    switch (_type) {
-      case -1:
-        break
-      case 1: console.log(obj.user.nickname + "加入了游戏")
-        break
-      case 0:
-        console.log(obj.operation.nickname + "移動至x:" + obj.operation.position.x + ",y:" + obj.operation.position.y)
-        this.maze.come(obj.operation.position.x, obj.operation.position.y)
-        break
-      case 200:
-        this.setState({
-          width: "1830",
-          height: "1230"
-        })
-        this.maze = new Extra.Maze(obj.maze, obj.location, obj.other, 30, 'game')
-        this.touch = new Extra.Touch(100, 100)
-        this.pushMsgQueue()
-        break
-      case 403:
-        console.log(obj.msg)
-        break
-      default: break
-    }
+	  	let _type = obj.code
+	    switch (_type) {
+	      case -1:
+	        break
+	      case 1: console.log(obj.user.nickname + "加入了游戏")
+	        break
+	      case 0:
+	        console.log(obj.operation.nickname + "移動至x:" + obj.operation.position.x + ",y:" + obj.operation.position.y)
+	        this.maze.come(obj.operation.position.x, obj.operation.position.y)
+	        break
+	      case 200:
+	        this.maze = new Extra.Maze(obj.maze, obj.location, obj.other, 30, 'game')
+	        this.touch = new Extra.Touch(100, 100)
+	        this.pushMsgQueue()
+	        break
+	      case 403:
+	        console.log(obj.msg)
+	        break
+	      default: break
+	    }
 	}
 
 	componentDidMount(){
@@ -99,13 +103,14 @@ class Main extends React.Component{
 		},()=>{
 			this.init(id,openid,nickname,headimg)
 		})
+		this.game=new Phaser.Game(375,667,Phaser.AUTO,'game',{preload:this.preload,create:this.create,update:this.update})
 	}
 
 	render(){
 		return(
-			<canvas style={{"backgroundImage":"url('./img/back.jpg') repeat"}} width={this.state.width||"100%"} height={this.state.height||"100%"} id="game">
-				
-			</canvas>
+			<div id="game">
+
+			</div>
 		)
 	}
 }
